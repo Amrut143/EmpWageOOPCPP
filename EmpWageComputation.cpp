7894b8f0ddf5466c1b9a3d5761816cdfafad723d
Amrut <amrutpanda7789@gmail.com>
2020-08-26 19:52:45 +0530

    ADDED: Code to compute employee wage using switch case

--- EmpWageComputation.cpp
+++ EmpWageComputation.cpp
@@ -6,28 +6,32 @@ using namespace std;
 class EmpWageComputation {
 
 	const int EMP_WAGE_PER_HOUR = 20;
-	const int PART_TIME = 1;
-	const int FULL_TIME = 2;
 
 	int empHrs, empWage = 0;
 
 	public:
 	void computeEmpWage() {
 
+		const int PART_TIME = 1;
+		const int FULL_TIME = 2;
+
 		srand(time(0));
 		int empCheck = rand() % 3;
 
-		if(empCheck == PART_TIME) {
-			empHrs = 4;
-		}
-		else if(empCheck == FULL_TIME) {
-			empHrs = 8;
-		}
-		else {
-			empHrs = 0;
+		switch(empCheck) {
+
+			case PART_TIME:
+					empHrs = 4;
+					break;
+
+			case FULL_TIME:
+					empHrs = 8;
+					break;
+
+			default:
+					empHrs = 0;
 		}
 		empWage = empHrs * EMP_WAGE_PER_HOUR;
-
 		cout << "Employee Daily Wage Is: " << empWage << endl;
 	}
 };
